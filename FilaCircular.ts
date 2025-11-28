54a3743f657b0dbe888c544e3bb5b173aa5c40bd
MailsonSousa88 <legionslmax86@gmail.com>
2025-11-28 09:55:26 -0300

    feat: Adicionado versão 3.0 da fila circular

--- FilaCircular.ts
+++ FilaCircular.ts
@@ -2,6 +2,9 @@ class FilaCircular {
 
     private elementos : Array<number>;
     private capacidadeDaLista : number;
+    private ponteiroFrente : number = 0;
+    private ponteiroTras : number = 0;
+    private tamanhoAtual : number = 0;
 
     constructor(capacidadeTotal:number){
         this.elementos = [];
@@ -10,11 +13,16 @@ class FilaCircular {
 
     //Metodo para adicionar elementos a lista(FILA)
     adicionarElementos(novoElemento:number): void{
-        if(this.elementos.length == this.capacidadeDaLista){
-            console.log(`Não é possivel armazenar elementos acima da capacidade total: ${this.capacidadeDaLista}`);
-        }else{
-            this.elementos.push(novoElemento);
-        }
+      if(this.tamanhoAtual == this.capacidadeDaLista){
+        console.log(`Não é possivel armazenar elementos acima da capacidade total: ${this.capacidadeDaLista}`);
+        return;
+      }
+
+        this.elementos[this.ponteiroTras] = novoElemento;
+        this.ponteiroTras = (this.ponteiroTras + 1) % this.capacidadeDaLista;
+        this.tamanhoAtual++;
+
+        console.log(`Adicionado elemento: ${novoElemento}`);
     }
 
     //Metodo para remover elementos da lista(FILA)
@@ -22,66 +30,77 @@ class FilaCircular {
         if(this.verificarStatusFilaVazia()){
             console.log("Não há números para remover...");
             return null;
-        }else{
-            let elementoRemovido: number = this.elementos.shift()!;
-            return elementoRemovido;
         }
+
+        let elementoRemovido: number = this.elementos[this.ponteiroFrente];
+        this.ponteiroFrente = (this.ponteiroFrente + 1) % this.capacidadeDaLista;
+        this.tamanhoAtual--;
+
+        console.log(`O elemento ${elementoRemovido} foi removido!`);
+        return elementoRemovido;
+        
     }
 
 
     //Metodos para exibir os elementos da FILA
     exibirQuantidadeElementos(): void{
-        
+        //Parte do Deric
     }
 
     exibirPrimeiroElemento(): number | null{
-        if(this.elementos.length == 0){
+        if(this.tamanhoAtual == 0){
             this.verificarStatusFilaVazia();
             return null;
         }else{
-            let primeiroElememto: number = this.elementos[0];
-            console.log(`O primeiro elementos da fila: ${primeiroElememto}`);
-            return primeiroElememto;
+            let primeiroElemento: number = this.elementos[this.ponteiroFrente];
+            console.log(`O primeiro elemento da fila: ${primeiroElemento}`);
+
+            return primeiroElemento;
         }
     }
 
     exibirElementosDaFila(): void{
-        
+        if(this.verificarStatusFilaVazia()){
+            return;
+        }
+
+        console.log(`Elementos da fila:`);
+        let indiceAtual: number = this.ponteiroFrente;
+
+        for (let i = 0; i < this.tamanhoAtual; i++) {
+        console.log(`Posição ${indiceAtual}: ${this.elementos[indiceAtual]}`);
+        indiceAtual = (indiceAtual + 1) % this.capacidadeDaLista;
+        }
+
     }
 
     exibirCapacidadeMaxima(){
-
+        //Parte do Deric
     }
 
     exibirCapacidadeDisponivel(){
-        let capacidadeDisponivel : number =this.capacidadeDaLista - this.elementos.length;
+        let capacidadeDisponivel : number =this.capacidadeDaLista - this.tamanhoAtual;
         console.log(`A capacidade disponivel na lista e de: ${capacidadeDisponivel}`);
     }
 
 
     //Metodos para checar o STATUS da FILA
     verificarStatusFilaVazia(): boolean{
-        if(this.elementos.length == 0){
+        if(this.tamanhoAtual == 0){
             console.log(`A fila está completamente vazia...`);
             return true;
-        }else{
-            console.log(`A fila não está mais vazia`);
-            return false;
         }
+        return false;
     }
 
     verificarStatusFilaCheia(): boolean{
-        if(this.elementos.length == this.capacidadeDaLista){
+        if(this.tamanhoAtual == this.capacidadeDaLista){
             console.log(`A fila está cheia`);
             return true
-        }else{
-            console.log(`A fila não está cheia...`);
-            return false;
         }
+        return false;
     }
 }
 
 //Inicialização da Fila Circular
 let lista : FilaCircular = new FilaCircular(5);
-
-//Para checar os metodos utilize lista.metodoDesejado();
